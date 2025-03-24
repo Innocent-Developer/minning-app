069af8022b659ecf755f259b036eaa0eac17a01e
Abubakkar <abubakkarsajid4@gmail.com>
2025-03-24 15:20:50 +0500

    Add cors

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const dotenv = require("dotenv");
+const cors = require('cors');
 dotenv.config();
 const connectDB = require("./Database/dbconect");
 connectDB();
 const router = require("./Routes/routers");
+
+// Enable CORS for all origins
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allow all methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
+}));
+
 app.use(express.json());
 const updateAvailableBalance = require('./Accounts/updateBalance/updatebalance.js');
-
 
 app.use("/",router)
 updateAvailableBalance();
@@ -16,7 +24,6 @@ updateAvailableBalance();
 app.get("/", (req, res) =>{
     res.send("Backend Working");
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
